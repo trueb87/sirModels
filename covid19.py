d01f39278b172ac00c41812f136dca46ebbd423f
Tony Rueb <tonyrueb@gmail.com>
2020-03-29 21:11:26 -0500

    Fixed bugs and played with values. Values are not final

--- covid19.py
+++ covid19.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 # Total population, N.
 N = 5611000
 # Initial number of infected recovered, and deceased individuals, I0 and R0.
-I0, R0, D0 = 441, 0, 5
+I0, R0, D0 = 441, 0, 9
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0 - D0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma, theta = 0.4, 1./15, 0.3
+beta, gamma, theta = .3, 1./10, 0.015
 # A grid of time points (in days)
-t = np.linspace(0, 160, 160)
+t = np.linspace(0, 365, 365)
 
 
 # The SIR model differential equations.
@@ -32,17 +32,17 @@ S, I, R, D = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
+ax = fig.add_subplot(111, fc="white", axisbelow=True)
 ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
-ax.plot(t, D/1000, 'p', alpha=0.5, lw=2, label='Deceased')
+ax.plot(t, D/1000, 'k', alpha=0.5, lw=2, label='Deceased')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (1000s)')
-ax.set_ylim(0,1.2)
+ax.set_ylim(0, 10000)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
-ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+ax.grid(b=True, which='major', c='gray', lw=1, ls='-')
 legend = ax.legend()
 legend.get_frame().set_alpha(0.5)
 for spine in ('top', 'right', 'bottom', 'left'):
