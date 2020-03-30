3b8dfd1b6d9c5799f55d4b679dec68461b26d4c6
Tony Rueb <tonyrueb@gmail.com>
2020-03-29 22:27:00 -0500

    More clean up and added title

--- covid19.py
+++ covid19.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 
 # Total population, N.
 N = 5611000
-# Initial number of infected recovered, and deceased individuals, I0 and R0.
-I0, R0, D0 = 441, 0, 9
+# Initial number of infected recovered, and deceased individuals, I0, R0, and D0.
+I0 = 441
+R0 = 0
+D0 = 9
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0 - D0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma, theta = .3, 1./10, 0.015
+# Contact rate, recovered rate, and mortality rate
+contact_rate = 0.3
+recovered_rate = 1. / 14
+mortality_rate = 0.036
 # A grid of time points (in days)
-t = np.linspace(0, 365, 365)
+t = np.linspace(0, 100, 100)
 
 
 # The SIR model differential equations.
@@ -27,19 +31,20 @@ def deriv(y, t, N, beta, gamma, theta):
 # Initial conditions vector
 y0 = S0, I0, R0, D0
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma, theta))
+ret = odeint(deriv, y0, t, args=(N, contact_rate, recovered_rate, mortality_rate))
 S, I, R, D = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, fc="white", axisbelow=True)
+ax.set_title("SIRD model with social distancing")
 ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.plot(t, D/1000, 'k', alpha=0.5, lw=2, label='Deceased')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (1000s)')
-ax.set_ylim(0, 10000)
+ax.set_ylim(0, 6000)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='gray', lw=1, ls='-')
